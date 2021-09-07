d06a2cdbd4fa20243d6f07769ec29a9e66114825
moiss16 <54283459+moiss16@users.noreply.github.com>
2021-09-06 22:13:14 -0700

    Add files via upload

---
+++ practica7/layout/layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  layout
+//
+//  Created by Alumno on 9/6/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
